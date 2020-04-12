db6251e6b931df410da78e60afb16ef8a978b86a
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-12 20:43:05 +0700

    Use key-value instead of hardcode string

---
+++ js-jest/src/item_name.js
@@ -0,0 +1,6 @@
+const ITEM_NAME = {
+  AGED_BRIE: 'Aged Brie',
+  BACKSTAGE_PASSES: 'Backstage passes to a TAFKAL80ETC concert',
+  SULFURAS: 'Sulfuras, Hand of Ragnaros'
+};
+exports.ITEM_NAME = ITEM_NAME;
