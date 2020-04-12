a027b3fafb178a62fff098beaa1947f9b31a9f05
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-12 20:57:07 +0700

    Refactor extract function and simplify expression

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -16,29 +16,25 @@ class Shop {
     for (let i = 0; i < this.items.length; i++) {
       const itemName = this.items[i].name;
       if (
-        itemName != ITEM_NAME.AGED_BRIE &&
-        itemName != ITEM_NAME.BACKSTAGE_PASSES
+        itemName == ITEM_NAME.AGED_BRIE ||
+        itemName == ITEM_NAME.BACKSTAGE_PASSES
       ) {
-        if (this.items[i].quality > 0) {
-          if (itemName != ITEM_NAME.SULFURAS) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
+        if (this.isQualityLessThan50(this.items[i].quality)) {
           this.items[i].quality = this.items[i].quality + 1;
           if (itemName == ITEM_NAME.BACKSTAGE_PASSES) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
+            if (this.isQualityLessThan50(this.items[i].quality)) {
+              if (this.items[i].sellIn < 11) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
+              if (this.items[i].sellIn < 6) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
           }
+        }
+      } else if (itemName != ITEM_NAME.SULFURAS) {
+        if (this.items[i].quality > 0) {
+          this.items[i].quality = this.items[i].quality - 1;
         }
       }
 
@@ -47,26 +43,25 @@ class Shop {
       }
 
       if (this.items[i].sellIn < 0) {
-        if (itemName != ITEM_NAME.AGED_BRIE) {
-          if (itemName != ITEM_NAME.BACKSTAGE_PASSES) {
-            if (this.items[i].quality > 0) {
-              if (itemName != ITEM_NAME.SULFURAS) {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
+        if (itemName == ITEM_NAME.AGED_BRIE) {
+          if (this.isQualityLessThan50(this.items[i].quality)) {
             this.items[i].quality = this.items[i].quality + 1;
+          }
+        } else if (itemName == ITEM_NAME.BACKSTAGE_PASSES) {
+          this.items[i].quality = 0;
+        } else if (itemName != ITEM_NAME.SULFURAS) {
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1;
           }
         }
       }
     }
 
     return this.items;
+  }
+
+  isQualityLessThan50(quality) {
+    return quality < 50;
   }
 }
 
